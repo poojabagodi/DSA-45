3d23e84dd3db09e01572d39621a3b0da10b5ad8e
Pooja Bagodi <poojabagodi23@gmail.com>
2025-07-06 22:46:06 +0530

    DAY 10

---
+++ DAY10/ReverseString.java
@@ -0,0 +1,17 @@
+package DAY10;
+
+public class ReverseString {
+    
+
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder sb=new StringBuilder("");
+       for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i != 0) sb.append(" ");
+        }
+
+        return sb.toString();
+    }
+}
+
