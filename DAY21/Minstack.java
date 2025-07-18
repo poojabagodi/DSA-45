baf26533ba0b8fa6eb7181471b65177024bfcad4
Pooja Bagodi <poojabagodi23@gmail.com>
2025-07-18 23:43:58 +0530

    DAY21

---
+++ DAY21/Minstack.java
@@ -0,0 +1,48 @@
+package DAY21;
+ import java.util.*;
+class MinStack {
+    Stack<Integer> s;
+    Stack<Integer> m;
+    public MinStack() {
+        s=new Stack<>();
+        m=new Stack<>();
+
+    }
+    
+    public void push(int val) {
+        s.push(val);
+        if(m.isEmpty() || val<=m.peek()){
+            m.push(val);
+        }
+        // else{
+        //     m.push(m.peek());
+        // }
+    }
+    
+    public void pop() {
+        int removed=s.pop();
+        if(removed==m.peek()){
+                  m.pop();
+        }
+     
+    }
+    
+    public int top() {
+        return s.peek();
+    }
+    
+    public int getMin() {
+         return  m.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */ {
+    
+}
