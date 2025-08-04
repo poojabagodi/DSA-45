c430edadb7f16fd9c0f16ebd0164f8a392aee96e
Pooja Bagodi <poojabagodi23@gmail.com>
2025-08-04 19:01:56 +0530

    day34

---
+++ DAY34/twosum.java
@@ -0,0 +1,15 @@
+package DAY34;
+import java.util.*;
+public class twosum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int com=target-nums[i];
+            if(map.containsKey(com)){
+                return new int[]{map.get(com), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1}; // If no solution found
+    }
+}
