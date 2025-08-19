aa88e5102bd367fd6cad97dca79ad00e0d533af2
Pooja Bagodi <poojabagodi23@gmail.com>
2025-08-19 21:32:08 +0530

    DAY44

---
+++ DAY44/countbits.java
@@ -0,0 +1,19 @@
+package DAY44;
+
+public class countbits {
+    public int[] countBits(int n) {
+        int arr[]=new int[n+1];
+        for(int i=1;i<=n;i++){
+            int r=i;
+            while(r!=0){
+                if((r&1)!=0){
+                    arr[i]++;
+                }
+
+                r=r>>1;
+            }
+
+        }
+        return arr;
+    }
+}
