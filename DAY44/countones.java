f7063c74cba8036197742df5d601b338f9f4fb7e
Pooja Bagodi <poojabagodi23@gmail.com>
2025-08-19 21:22:02 +0530

    DAY44

---
+++ DAY44/countones.java
@@ -0,0 +1,14 @@
+package DAY44;
+
+public class countones {
+      public int hammingWeight(int n) {
+        int count=0;
+        while(n!=0){
+           if((n & 1 ) != 0){
+            count++;
+           }
+           n=n>>1;
+        }
+        return count;
+    }
+}
